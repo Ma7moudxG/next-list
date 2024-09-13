b9e079e5f4810f62b8e364d7d856dc5bc7b37582
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-09-14 02:44:33 +0300

    Customize Radix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,15 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import NavBar from "./NavBar";
 import '@radix-ui/themes/styles.css';
-import {Theme} from '@radix-ui/themes'
+import './theme-config.css'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from 'next/font/google';
+import {Theme} from '@radix-ui/themes';
+import NavBar from "./NavBar";
 
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -28,11 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme accentColor="violet">
           <NavBar />
           <main className="p-5">
             {children}
